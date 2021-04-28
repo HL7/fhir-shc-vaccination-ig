d7db49edd64f947c2b78b130005ae66458a11d39
Neelima Karipineni <1413472+nkarip@users.noreply.github.com>
2021-04-28 14:54:07 -0400

    manually filter errors, add expected error testing

--- src/test/java/dvci/vaccineCredentialIg/ProfileTest.java
+++ src/test/java/dvci/vaccineCredentialIg/ProfileTest.java
@@ -3,6 +3,7 @@ package dvci.vaccineCredentialIg;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
@@ -21,13 +22,15 @@ import org.hl7.fhir.validation.ValidationEngine;
 
 public class ProfileTest {
     private static final String DEF_TX = "http://tx.fhir.org";
+    private static String DEF_TXLOG = null;
     private static ValidationEngine validator;
-    private static String txLog = null;
     private static final Logger logger = LoggerFactory.getLogger(ProfileTest.class);
 
     @BeforeAll
     public static void setup() throws Exception {
-        validator = new ValidationEngine("hl7.fhir.r4.core#4.0.1", DEF_TX, txLog, FhirPublication.R4, true, "4.0.1");
+        String txsrvr = System.getProperty("txsrvr") != null ? System.getProperty("txsrvr"): DEF_TX;
+        String txLog = System.getProperty("txLog") != null ? System.getProperty("txLog"): DEF_TXLOG;
+        validator = new ValidationEngine("hl7.fhir.r4.core#4.0.1", txsrvr, txLog, FhirPublication.R4, true, "4.0.1");
         validator.getContext().loadFromFolder("fsh-generated/resources");
     }
 
@@ -39,25 +42,102 @@ public class ProfileTest {
         profiles.add(profile);
         OperationOutcome outcome = validator.validate(source, profiles);
 
-        int countErrors = 0;
-        StringBuilder errors = new StringBuilder();
+        ArrayList<OperationOutcomeIssueComponent> errors = filterErrors(outcome);
+        StringBuilder errorMessages = new StringBuilder();
         StringBuilder messages = new StringBuilder();
-        for (OperationOutcomeIssueComponent issue : outcome.getIssue()) {
+        outcome.getIssue().forEach(issue -> {
             messages.append(issue.getUserData("source.vm"));
             messages.append("\n");
-            if (issue.getSeverity().equals(IssueSeverity.ERROR)) {
-                countErrors++;
-                errors.append(issue.getUserData("source.vm"));
-                errors.append("\n");
-            }
-        }
+        });
+        errors.forEach(error -> {
+            errorMessages.append(error.getUserData("source.vm"));
+            errorMessages.append("\n");
+        });
 
         if (outcome.getIssue().size() > 0) {
             logger.info("Issues for {},  profile {}", source, profile);
             logger.info(messages.toString());
         }
-        if (countErrors > 0) {
-            Assertions.fail("source: " + source + ", profile: " + profile + "\n" + errors.toString());
+        if (errors.size() > 0) {
+            Assertions.fail("source: " + source + ", profile: " + profile + "\n" + errorMessages.toString());
         }
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/tests_errors.csv", numLinesToSkip = 1)
+    public void testValidationExpectedErrors(String source, String profile, IssueSeverity expectedIssueSeverity, String expectedLocation, String expectedErrorMessage)
+            throws FHIRException, IOException, EOperationOutcome {
+        List<String> profiles = new ArrayList<>();
+        profiles.add(profile);
+        OperationOutcome outcome = validator.validate(source, profiles);
+
+        Assertions.assertTrue(outcome.getIssue()
+            .stream()
+            .filter(issue -> expectedIssueSeverity == null || issue.getSeverity().equals(expectedIssueSeverity))
+            .filter(issue-> expectedErrorMessage == null || issue.getDetails().getText().contains(expectedErrorMessage))
+            .filter(issue -> expectedLocation == null || issue.getExpression()
+                .stream()
+                .filter(s -> s.asStringValue().contains(expectedLocation))
+                .collect(Collectors.toList())
+                .size() > 0)
+            .collect(Collectors.toList())
+            .size() > 0,
+            "Should throw level=" + expectedIssueSeverity + ", location=" + expectedLocation + ", message=" + expectedErrorMessage
+        );
+    }
+
+    private ArrayList<OperationOutcomeIssueComponent> filterErrors(OperationOutcome outcome){
+        // manually filter out errors that are due to validator inconsistencies
+        ArrayList<OperationOutcomeIssueComponent> errors = new ArrayList<>();
+        for (OperationOutcomeIssueComponent issue : outcome.getIssue()) {
+            if (issue.getSeverity().equals(IssueSeverity.ERROR)){
+                if(ignoreError(issue)){
+            
+                }
+                else if(issue.getDetails().getText()
+                        .contains("Bundle.entry:vaccinationCredentialImmunization: minimum required = 1, but only found 0")){
+                    //if there is an Immunization entry with errors, then ignore this error        
+                    if(!findImmunizationError(outcome)){
+                        errors.add(issue);
+                    }
+                }
+                else {
+                    errors.add(issue);
+                }
+            }
+        }
+
+        return errors;
+    }
+
+    private boolean ignoreError(OperationOutcomeIssueComponent issue){
+        if(issue.getDetails().getText()
+                .equalsIgnoreCase("Relative URLs must be of the format [ResourceName]/[id].  Encountered resource:0")){
+            return true;
+        }
+        else if(issue.getDetails().getText()
+            .equalsIgnoreCase("Resource requires an id, but none is present")){
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean findImmunizationError(OperationOutcome outcome){
+        // find error entry for Immunization resource type that has an ignored error
+        for (OperationOutcomeIssueComponent issue : outcome.getIssue()) {
+            if(issue.getSeverity().equals(IssueSeverity.ERROR)){
+                if(issue.getExpression()
+                        .stream()
+                        .filter(s -> s.asStringValue().contains("resource.ofType(Immunization)"))
+                        .collect(Collectors.toList())
+                        .size() > 0){
+                    if(ignoreError(issue)){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
